092ad1706773e3b258767841d2ae1b6b22ecda24
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-27 18:36:27 +0200

    feat: added created at to user object

--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -6,6 +6,7 @@ const User = mongoose.model(
     username: String,
     email: String,
     password: String,
+    createdAt: Date,
     roles: [
       {
         type: mongoose.Schema.Types.ObjectId,
