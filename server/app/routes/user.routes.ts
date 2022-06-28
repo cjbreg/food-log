c4ba97b2bbf3c6daf02c2885463d03e254824411
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-28 13:20:02 +0200

    feat: added trycatches to user routes

--- server/app/routes/user.routes.ts
+++ server/app/routes/user.routes.ts
@@ -5,19 +5,35 @@ const router = express.Router();
 const controller = new UserController();
 
 router.get("/user", async (req: Request, res: Response) => {
-  controller.fetchAll(req, res);
+  try {
+    controller.fetchAll(req, res);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 });
 
 router.get("/user/:id", async (req: Request, res: Response) => {
-  controller.fetchById(req, res);
+  try {
+    controller.fetchById(req, res);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 });
 
 router.put("/user/:id", async (req: Request, res: Response) => {
-  controller.updateById(req, res);
+  try {
+    controller.updateById(req, res);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 });
 
 router.delete("/user/:id", async (req: Request, res: Response) => {
-  controller.deleteById(req, res);
+  try {
+    controller.deleteById(req, res);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 });
 
 export default router;
