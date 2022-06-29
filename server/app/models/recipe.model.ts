65ca499bcba2f9a99bbcb3734ee101f90ec8eb79
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-29 17:20:54 +0200

    feat: updated model

--- server/app/models/recipe.model.ts
+++ server/app/models/recipe.model.ts
@@ -4,15 +4,17 @@ export interface IRecipe extends Document {
   userId: Types.ObjectId[];
   name: string;
   originalSource: string;
+  createdAt: Date;
 }
 
 const RecipeSchema: Schema = new Schema({
-  userId: {
+  user: {
     type: Types.ObjectId,
     ref: "User",
   },
   name: { type: String, required: true },
   originalSource: { type: String, required: true },
+  createdAt: { type: Date, required: true },
 });
 
 export default model<IRecipe>("Recipe", RecipeSchema);
