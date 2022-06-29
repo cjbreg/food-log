3331b686c01deb806f74f98b0174961882feefd5
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-29 10:46:18 +0200

    feat: added signin/up

--- server/app/controllers/auth.controller.ts
+++ server/app/controllers/auth.controller.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
+import Role, { IRole } from "../models/role.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 interface AuthInterface {
   signup(req: Request, res: Response): Promise<void>;
@@ -10,11 +14,71 @@ class AuthController implements AuthInterface {
   constructor() {}
 
   signup = async (req: Request, res: Response): Promise<void> => {
-    res.json({ message: "This is the signup route!" });
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+      createdAt: Date.now(),
+    });
+
+    user.save((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      Role.findOne({ name: "user" }, (err: any, role: IRole) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        user.roles = [role._id];
+        user.save((err) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          res.send({ message: "User was registered successfully!" });
+        });
+      });
+    });
   };
 
   signin = async (req: Request, res: Response): Promise<void> => {
-    res.json({ message: "This is the signin route!" });
+    const { username, password } = req.body;
+
+    await User.findOne({ username }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (!user) {
+        return res
+          .status(404)
+          .send({ message: "Invalid Username or Password!" });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(password, user.password);
+
+      if (!passwordIsValid) {
+        return res
+          .status(401)
+          .send({ message: "Invalid Username or Password!" });
+      }
+
+      const secretKey = process.env.SECRET_KEY || "";
+
+      var accessToken = jwt.sign({ id: user.id }, secretKey);
+
+      res.json({
+        username,
+        userID: user.id,
+        accessToken,
+      });
+    });
   };
 
   signout = async (req: Request, res: Response): Promise<void> => {
