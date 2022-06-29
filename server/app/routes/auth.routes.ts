48576ab0d64909487810c837612c170d6c543cf3
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-29 15:00:52 +0200

    feat: implemented middleware

--- server/app/routes/auth.routes.ts
+++ server/app/routes/auth.routes.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import AuthController from "../controllers/auth.controller";
+import {
+  checkDuplicateUsernameOrEmail,
+  checkRolesExisted,
+} from "../middleware/verifySignUp";
 
 const router = express.Router();
 const controller = new AuthController();
@@ -9,13 +13,17 @@ router.use("/api/auth", (req, res, next) => {
   next();
 });
 
-router.post("/auth/signup", async (req: Request, res: Response) => {
-  try {
-    controller.signup(req, res);
-  } catch (error: any) {
-    console.error(error.message);
+router.post(
+  "/auth/signup",
+  [checkDuplicateUsernameOrEmail, checkRolesExisted],
+  async (req: Request, res: Response) => {
+    try {
+      controller.signup(req, res);
+    } catch (error: any) {
+      console.error(error.message);
+    }
   }
-});
+);
 
 router.post("/auth/signin", (req: Request, res: Response) => {
   try {
