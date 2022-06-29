3140219a87daddd5411571140939c3900792caca
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-29 10:46:33 +0200

    fix: correct date object

--- server/app/models/user.model.ts
+++ server/app/models/user.model.ts
@@ -4,7 +4,7 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  createdAt: number;
+  createdAt: Date;
   roles: Types.ObjectId[];
 }
 
@@ -12,7 +12,7 @@ const UserSchema: Schema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  createdAt: { type: Number, required: true },
+  createdAt: { type: Date, required: true },
   roles: [
     {
       type: Types.ObjectId,
