51a0bfade9440b6d37e9e4246211344a29ac7426
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-29 10:52:04 +0200

    feat: working auth check

--- server/app/middleware/authJwt.ts
+++ server/app/middleware/authJwt.ts
@@ -4,23 +4,46 @@ import User from "../models/user.model";
 import Role from "../models/role.model";
 import { Request, Response, NextFunction } from "express";
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  let token = req.session.token;
+// const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+//   let token = req.beare;
+//   if (!token) {
+//     return res.status(403).send({ message: "No token provided!" });
+//   }
 
-  if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
+//   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "Unauthorized!" });
+//     }
+//     req.body.userId = decoded.id;
+//     next();
+//   });
+// };
+
+export const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  const secretKey = process.env.SECRET_KEY || "";
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.body.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
   }
-
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
-    }
-    req.body.userId = decoded.id;
-    next();
-  });
 };
 
-const authJwt = {
-  verifyToken,
-};
-module.exports = authJwt;
+// const authJwt = {
+//   verifyToken,
+// };
+// module.exports = authJwt;
